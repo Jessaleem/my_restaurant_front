5f53af3fa24a3131cfdfbc8dac832b07f9adf5d6
Jessica <aleejess@gmail.com>
2022-09-20 15:05:24 -0500

    feat add pages login and register

--- components/Layout.js
+++ components/Layout.js
@@ -3,29 +3,29 @@ import Nav from './Nav';
 
 const Layout = ({ children }) => (
   <>
-  <Head>
-    <title>Menu Database</title>
-  </Head>
+    <Head>
+      <title>Menu Database</title>
+    </Head>
 
-  <Nav />
+    <Nav />
 
-  <main className="px-4">
-    <div
-      className="
-        flex
-        justify-center
-        items-center
-        bg-white
-        mx-auto
-        w-2/4
-        rounded-lg
-        my-16
-        p16
-      "
-    >
-      <div className="text-2xl font-medium">{children}</div>
-    </div>
-  </main>
+    <main className="px-4">
+      <div
+        className="
+          flex
+          justify-center
+          items-center
+          bg-white
+          mx-auto
+          w-2/4
+          rounded-lg
+          my-16
+          p16
+        "
+      >
+        <div className="text-2xl font-medium">{children}</div>
+      </div>
+    </main>
   </>
 );
 
---
+++ pages/menu.jsx
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+
+const MenuList = () => {
+  return(
+    <Layout>
+      <h1 className="text-5xl md:ext-6xl font-extrabold leading-tighter mb-4">
+        <span className="bg-clip-text tex-transparent bg-gradient-to-r from-blue-500 to-teal-400 py-2">
+          Menu
+        </span>
+      </h1>
+    </Layout>
+  );
+};
+
+export default MenuList;
